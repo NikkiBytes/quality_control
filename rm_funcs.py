eab123706af72087636236fc7d70699ce4bb5f40
gshearrer <grace.shearer@gmail.com>
2018-07-13 16:41:39 -0400

    changes

--- rm_funcs.py
+++ rm_funcs.py
@@ -9,32 +9,36 @@ Created on Wed Jul 11 15:4:58 2018
 """
 
 
-import os 
-import glob 
+import os
+import glob
 import shutil
 
 
 
 basepath = "/projects/niblab/bids_projects/Experiments/Bevel/data"
-derivpath = os.path.join(basepath, "derivatives", "missing_onsets")
+derivpath = os.path.join(basepath, "derivatives")
 
 subjects=glob.glob(os.path.join(derivpath, "sub-*"))
 
 
 funcs = glob.glob(os.path.join(derivpath, "sub-*", "func", "*nii.gz*"))
-    
+
 for funcpath in funcs:
     print(funcpath)
     os.remove(funcpath)
-    
-    
+
+
 motions = glob.glob(os.path.join(derivpath, "sub-*", "func", "motion_assessment", "*txt"))
 
 for motodir in motions:
     print(motodir)
     os.remove(motodir)
-    
+
 pngs = glob.glob(os.path.join(derivpath, "sub-*", "func", "motion_assessment", "*png"))
+
+for img in pngs:
+    print(img)
+    os.remove(img)
 
 analysis = glob.glob(os.path.join(derivpath, "sub-*", "func", "Analysis", "feat1", "*feat1"))
 
